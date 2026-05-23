56d1c0a4ca8afed4c8f0b1eb94aa6d39ea60ee06
YuriiZnakharchuk <whyzedcs@gmail.com>
2026-05-23 22:12:21 +0300

    Initial Commit

---
+++ CodefinityTestTask/CodefinityTestTaskApp.swift
@@ -0,0 +1,17 @@
+//
+//  CodefinityTestTaskApp.swift
+//  CodefinityTestTask
+//
+//  Created by Yurii Znakharchuk on 23.05.2026.
+//
+
+import SwiftUI
+
+@main
+struct CodefinityTestTaskApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
